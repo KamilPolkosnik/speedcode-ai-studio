9c1158ef5330fc992e7166941b57c6e3a459650e
AureliuszS1 <123911273+AureliuszS1@users.noreply.github.com>
2025-09-12 22:36:07 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -24,4 +24,21 @@ const App = () => (
   </QueryClientProvider>
 );
 
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
 export default App;
+
